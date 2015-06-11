b4013a112052d1f79f3114eb418d14c478c9c9c2
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-06-10 22:44:16 -0600

    Added routes for user registration and auth, index for journal entries resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
 
   root "static_pages#index"
 
+  get "sign-up", to: "registrations#new"
+  post "sign-up", to: "registrations#create"
+  get "sign-out", to: "authentication#destroy"
+  get "sign-in", to: "authentication#new"
+  post "sign-in", to: "authentication#create"
+
+  resources :journal_entries, only: [:index]
+
 end
